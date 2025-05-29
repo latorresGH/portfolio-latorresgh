b26c22c570c1f350ec9cf5eacbc8bd785aa5b0f8
laTorresGH <laureano26112001@hotmail.com>
2025-05-29 03:24:48 -0300

    Add MyExperience component with layout and content for professional experiences

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      './src/**/*.{astro,html,js,jsx,ts,tsx}', // para escanear todos los archivos
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          suisse: ['SuisseIntl', 'sans-serif'],
-          aurora: ['Aurora', 'sans-serif'],
-          Zaslia: ['Zaslia', 'sans-serif'],
-          montserrat: ['Montserrat', 'sans-serif'],
-          overcame: ['OvercameDemo', 'sans-serif'], 
-          caslon: ['"Libre Caslon Text"', 'serif'],
-        },
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // para escanear todos los archivos
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        suisse: ["SuisseIntl", "sans-serif"],
+        aurora: ["Aurora", "sans-serif"],
+        Zaslia: ["Zaslia", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        overcame: ["OvercameDemo", "sans-serif"],
+        caslon: ['"Libre Caslon Text"', "serif"],
+        FuturaBook: ["FuturaBook", "sans-serif"],
+      },
+      lineHeight: {
+        custom: "1.8125rem", // para 29px
+      },
+      backgroundSize: {
+        50: "50%",
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
